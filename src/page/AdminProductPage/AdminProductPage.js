44257e46f57094116f7759a036370064b0e5cc96
jamesjo <jamescakr@gmail.com>
2025-08-06 18:48:57 -0600

    update admin page

--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -28,6 +28,9 @@ const AdminProductPage = () => {
   const tableHeader = ["#", "Sku", "Name", "Price", "Stock", "Image", "Status", ""];
 
   //상품리스트 가져오기 (url쿼리 맞춰서)
+  useEffect(() => {
+    dispatch(getProductList());
+  }, []);
 
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
